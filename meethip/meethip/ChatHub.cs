74e5b7f54eff7748e454d7c7f5efd6dd59bd6f76
Jouni Mietola <jouni.mietola@gmail.com>
2017-10-16 12:43:50 +0300

    Initial Commit

---
+++ meethip/meethip/ChatHub.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Device.Location;
+using System.Data.SqlTypes;
+using System.Threading.Tasks;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+
+        }
+
+
+        public async Task Locations(string name, string latitude, string longitude)
+        {
+            // Call the broadcastMessage method to update clients.
+
+            var conn = ConfigurationManager.ConnectionStrings["AzureConnectionString"];
+            string connString = conn.ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+
+                int userid = 0;
+                bool match = false;
+                int locationuserid = 0;
+                //
+                // Open the SqlConnection.
+                //
+                con.Open();
+
+                //
+
+                string sql = "INSERT INTO users(name) VALUES(@param1)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@param1", name);
+
+                cmd.ExecuteNonQuery();
+
+                using (SqlCommand command = new SqlCommand("SELECT TOP 1 * FROM users ORDER BY userID DESC", con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        userid = reader.GetSqlInt32(0).Value;
+                    }
+                }
+
+                //
+
+                sql = "INSERT INTO rooms(userID, name) VALUES(@param1,@param2)";
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@param1", userid);
+                cmd.Parameters.AddWithValue("@param2", name + "'s room");
+                cmd.Parameters.AddWithValue("@param3", longitude);
+                cmd.ExecuteNonQuery();
+
+                //
+
+                sql = "INSERT INTO locations(userID, latitude,longitude) VALUES(@param1,@param2, @param3)";
+                cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@param1", userid);
+                cmd.Parameters.AddWithValue("@param2", latitude);
+                cmd.Parameters.AddWithValue("@param3", longitude);
+                cmd.ExecuteNonQuery();
+
+                var coordinateA = new GeoCoordinate(double.Parse(latitude), double.Parse(longitude));
+
+
+                string roomqueryresult = $"SELECT * FROM rooms WHERE userID = {userid}";
+                var initialroomid = 0;
+                using (SqlCommand command = new SqlCommand(roomqueryresult, con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        initialroomid = reader.GetSqlInt32(0).Value;
+                    }
+                }
+
+                using (SqlCommand command = new SqlCommand("SELECT * FROM locations", con))
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+
+                        locationuserid = reader.GetSqlInt32(1).Value;
+                        double latitude2 = reader.GetSqlDouble(2).Value;
+                        double longitude2 = reader.GetSqlDouble(3).Value;
+
+                        var coordinateB = new GeoCoordinate(latitude2, longitude2);
+
+                        var distance = coordinateB.GetDistanceTo(coordinateA);
+
+                        if (distance < 1000000 && distance > 1)
+                        {
+                            match = true;
+                            break;
+                        }
+                    }
+
+                }
+
+                if (match)
+                {
+                    // emit client found and add user to room
+                    int roomID = 0;
+
+                    string result = $"SELECT * FROM rooms WHERE userID = {locationuserid}";
+
+                    using (SqlCommand command = new SqlCommand(result, con))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            roomID = reader.GetSqlInt32(0).Value;
+                        }
+                    }
+
+                    // join room and emit message to room
+                    await JoinRoom(Convert.ToString(roomID));
+                    Clients.Group(Convert.ToString(roomID)).addChatMessage(Context.User.Identity.Name + " joined.");
+                }
+                else
+                {
+                    int roomID = 0;
+                    string result = $"SELECT * FROM rooms WHERE userID = {locationuserid}";
+
+                    using (SqlCommand command = new SqlCommand(result, con))
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            roomID = reader.GetSqlInt32(0).Value;
+                        }
+                    }
+
+                    // join own room first
+                    await JoinRoom(Convert.ToString(initialroomid));
+                }
+
+
+            }
+        }
+
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
+        }
+    }
+
+}
+
---
+++ meethip/meethip/Scripts/Main.jsx
@@ -0,0 +1,143 @@
+﻿
+/*$(function () {
+    // Declare a proxy to reference the hub.
+    var chat = $.connection.chatHub;
+    // Create a function that the hub can call to broadcast messages.
+    chat.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message.
+        var encodedName = $('<div />').text(name).html();
+        var encodedMsg = $('<div />').text(message).html();
+        // Add the message to the page.
+        $('#discussion').append('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+    $('#message').focus();
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+});
+*/
+// Declare a proxy to reference the hub.
+var chat = $.connection.chatHub;
+
+var CommentForm = React.createClass({
+
+    componentDidMount: function () {
+
+        // Create a function that the hub can call to broadcast messages.
+        chat.client.addChatMessage = function (message) {
+            // Html encode display name and message.
+            var encodedMsg = $('<div />').text(message).html();
+            // Add the message to the page.
+            $('#discussion').append('<li>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+        };
+
+
+
+
+        // Create a function that the hub can call to broadcast messages.
+        chat.client.broadcastMessage = function (name, message) {
+            // Html encode display name and message.
+            var encodedName = $('<div />').text(name).html();
+            var encodedMsg = $('<div />').text(message).html();
+            // Add the message to the page.
+            $('#discussion').append('<li><strong>' + encodedName
+                + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+        };
+        // Get the user name and store it to prepend to messages.
+        $('#displayname').val(prompt('Enter your name:', ''));
+        // Set initial focus to message input box.
+        $('#message').focus();
+        // Start the connection.
+        $.connection.hub.start().done(function () {
+            $('#sendmessage').click(function () {
+                // Call the Send method on the hub.
+                chat.server.send($('#displayname').val(), $('#message').val());
+                // Clear text box and reset focus for next comment.
+                $('#message').val('').focus();
+            });
+
+
+
+        });
+    },
+
+    render: function () {
+        return (
+            <div className="container">
+                <input type="text" id="message" />
+                <input type="button" id="sendmessage" value="Send" />
+                <input type="hidden" id="displayname" />
+                <ul id="discussion"></ul>
+      </div>
+        );
+    }
+});
+
+
+
+var CommentBox = React.createClass({
+
+    getInitialState: function () {
+        return {
+            latitude: 66,
+            longitude: 66,
+            hipFound: false,
+            error: null };
+    },
+
+    componentDidMount: function () {
+
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+
+                $.connection.hub.start().done(function () {
+                    // Call the Locations method on the hub.
+                    
+                    chat.server.locations($('#displayname').val(), position.coords.latitude, position.coords.longitude);
+                });
+
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+
+
+    },
+
+
+    render: function () {
+        return (
+        <div className="CommentBox">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to MeetHip</h1>
+        </header>
+        <p>Latitude: {this.state.latitude}</p>
+        <p>Longitude: {this.state.longitude}</p>
+        {this.state.error ? <p>Error: {this.state.error}</p> : null}
+
+          <p>No hips found</p>
+          <CommentForm />
+      </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
